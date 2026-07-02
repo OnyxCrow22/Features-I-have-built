184c4b3ed8fa6522fc8d196b37e25f924ba25314
HybridDeveloper291 <102984597+OnyxCrow22@users.noreply.github.com>
2026-07-02 18:12:50 +0100

    Delete Bots directory

--- Bots/shared_utility.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-def send_discord_alert(webhook_url, message):
-    payload = {"content": message}
-    try:
-        requests.post(webhook_url, json=payload)
-
-    except Exception as e:
-        print(f"Failed to send Discord alert!")
